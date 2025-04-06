96629bec9d6dac471a7539309022884150ea5d81
poriyaASadi <133978734+poriyaASadi@users.noreply.github.com>
2025-04-06 19:18:23 +0330

    add ocd

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import '../node_modules/aos/dist/aos.js'
-import '../node_modules/aos/dist/aos.css'
-createApp(App).mount('#app');
-AOS.init();
